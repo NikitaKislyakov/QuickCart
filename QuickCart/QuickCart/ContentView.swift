c92fb8a47a2bf4d1482a9bbfa093cff1c0885a00
Nikita Kislyakov <nikitakislyakovv@gmail.com>
2025-02-11 12:06:18 +0300

    Project setup

---
+++ QuickCart/QuickCart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickCart
+//
+//  Created by Никита Кисляков on 11.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickCart/QuickCart/QuickCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCartApp.swift
+//  QuickCart
+//
+//  Created by Никита Кисляков on 11.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuickCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
